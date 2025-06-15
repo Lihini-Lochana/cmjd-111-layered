cb4bba7f17f4ce5a269737e2cea832cbebcb9422
Lihini-Lochana <lihinilochana18@gmail.com>
2025-06-15 09:21:31 +0530

    first commit

---
+++ src/edu/ijse/layered/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package edu.ijse.layered;
+import edu.ijse.layered.db.DBConnection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Lihini
+ */
+public class Main {
+
+    
+    public static void main(String[] args)  throws ClassNotFoundException, SQLException  {
+         DBConnection.getInstance().getConnection();
+    }
+    
+}
